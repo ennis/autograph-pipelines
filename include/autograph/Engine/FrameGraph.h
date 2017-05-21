70ff1d756d9bb98e55c00c961f6567a2e0fb826d
Alexandre Bléron <alex.bleron@gmail.com>
2017-05-22 00:01:38 +0200

    stuff

--- include/autograph/Engine/FrameGraph.h
+++ include/autograph/Engine/FrameGraph.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <any>
+#include <autograph/Core/Support/Debug.h>
 #include <autograph/Core/Support/HashCombine.h>
 #include <autograph/Core/Support/SmallVector.h>
-#include <autograph/Gfx/Texture.h>
+#include <autograph/Core/Support/Variant.h>
 #include <autograph/Gfx/Buffer.h>
+#include <autograph/Gfx/Texture.h>
 #include <functional>
 #include <unordered_map>
 #include <vector>
@@ -95,20 +97,34 @@ public:
   };
 
   struct TextureResourceDesc {
-	  Texture::Desc desc;
+    Texture::Desc desc;
   };
 
   struct BufferResourceDesc {
-	  size_t size;
-	  BufferUsage usage;
+    size_t size;
+    BufferUsage usage;
   };
 
-  struct ResourceDesc {
-	  int handle = -1;
-	  int lifetimeBegin = 0;
-	  int lifetimeEnd = 0;
-	  int allocatedResource = -1;
-	  ag::variant<TextureResourceDesc, BufferResourceDesc> desc;
+
+  struct ResourceDesc
+  {
+    int handle = -1;
+    int lifetimeBegin = 0;
+    int lifetimeEnd = 0;
+
+	struct Texture
+	{
+		ag::Texture::Desc desc;
+		ag::Texture* ptex;
+	};
+	struct Buffer
+	{
+		size_t size;
+		ag::BufferUsage usage;
+		ag::Buffer* buf;
+	};
+
+	ag::variant<Texture, Buffer> v;
   };
 
   ////////////////////////////////////////////
@@ -147,52 +163,52 @@ public:
       d.mipMapCount = mipMaps.count;
       d.sampleCount = ms.count;
       d.opts = opts;
-      ResourceDesc rd;
-      rd.res = TextureResource{d, -1};
-	  Resource r = frameGraph.addResourceDesc(rd);
+	  ResourceDesc rd;
+      rd.desc = TextureResourceDesc{d};
+      Resource r = frameGraph.addResourceDesc(rd);
       pass.creates.push_back(r);
-	  //pass.writes.push_back(r);
+      // pass.writes.push_back(r);
       AG_DEBUG("[FrameGraph] createTexture2D {}.{}", r.handle, r.renameIndex);
       return r;
     }
 
     Resource read(Resource in) {
       AG_DEBUG("[FrameGraph] read {}.{}", in.handle, in.renameIndex);
-	  pass.reads.push_back(in);
+      pass.reads.push_back(in);
       return in;
     }
 
     Resource write(Resource out) {
       AG_DEBUG("[FrameGraph] write {}.{}", out.handle, out.renameIndex);
       // same resource, bump the rename index
-	  pass.writes.push_back(out);
+      pass.writes.push_back(out);
       return Resource{out.handle, out.renameIndex + 1};
     }
 
-	Resource copy(Resource in) {
-		const ResourceDesc& rd = frameGraph.getResourceDesc(in.handle);
-		Resource out = frameGraph.addResourceDesc(rd);
-		pass.creates.push_back(out);
-		return out;
-	}
+    Resource copy(Resource in) {
+      const ResourceDesc &rd = frameGraph.getResourceDesc(in.handle);
+      Resource out = frameGraph.addResourceDesc(rd);
+      pass.creates.push_back(out);
+      return out;
+    }
 
-	bool isTexture(Resource in) const {
-		return frameGraph.isTexture(in.handle);
-	}
+    bool isTexture(Resource in) const {
+      return frameGraph.isTexture(in.handle);
+    }
 
-	Texture::Desc getTextureDesc(Resource in) const {
-		return frameGraph.getTextureDesc(in.handle);
-	}
+    Texture::Desc getTextureDesc(Resource in) const {
+      return frameGraph.getTextureDesc(in.handle);
+    }
 
-	bool isBuffer(Resource in) const { return frameGraph.isBuffer(in.handle); }
+    bool isBuffer(Resource in) const { return frameGraph.isBuffer(in.handle); }
 
-	BufferResourceDesc getBufferDesc(Resource in) const {
-		return frameGraph.getBufferSize(in.handle);
-	}
+    BufferResourceDesc getBufferDesc(Resource in) const {
+      return frameGraph.getBufferSize(in.handle);
+    }
 
-	ResourceDesc getResourceDesc(Resource in) const {
-		return frameGraph.getResourceDesc(in.handle);
-	}
+    ResourceDesc getResourceDesc(Resource in) const {
+      return frameGraph.getResourceDesc(in.handle);
+    }
 
     void setName(const char *name_) { pass.name = name_; }
 
@@ -226,7 +242,7 @@ public:
     PassBuilder pb{*this, p};
     setup(pb, *pdata);
     // add pass to list
-	AG_DEBUG("-> Add pass {}", p.name.c_str());
+    AG_DEBUG("-> Add pass {}", p.name.c_str());
     passes.push_back(std::move(p));
     return *pdata;
   }
@@ -235,60 +251,36 @@ public:
 
   ////////////////////////////////////////////
 private:
-	Resource addResourceDesc(const ResourceDesc &rd);
-	const ResourceDesc& getResourceDesc(int handle);
-	const Texture::Desc& getTextureDesc(int handle) {
-	  auto &rd = getResourceDesc();
-	  auto ptexdesc = ag::get_if<TextureResourceDesc>(&rd.desc);
-	  if (!ptexdesc)
-		  ag::failWith("");
+  Resource addResourceDesc(const ResourceDesc &rd);
+  const ResourceDesc &getResourceDesc(int handle) const;
+
+  const Texture::Desc &getTextureDesc(int handle) const {
+    auto &rd = getResourceDesc(handle);
+    auto ptexdesc = ag::get_if<TextureResourceDesc>(&rd.desc);
+    if (!ptexdesc)
+      ag::failWith("Not a texture");
+    return ptexdesc->desc;
   }
+
+  const BufferResourceDesc &getBufferDesc(int handle) const {
+    auto &rd = getResourceDesc(handle);
+    auto bufdesc = ag::get_if<BufferResourceDesc>(&rd.desc);
+    if (!bufdesc)
+      ag::failWith("Not a buffer");
+    return *bufdesc;
+  }
+
+  int FrameGraph::assignCompatibleResource(ResourceDesc &desc, int passId);
 
   // virtual resources (can be aliased)
   std::vector<std::unique_ptr<ResourceDesc>> resources;
+  // we can keep the list from one frame to another,
+  // although I suspect that re-allocating memory for resources
+  // on the GPU should not be that costly
+  std::vector<std::unique_ptr<Texture>> textures;
+  std::vector<std::unique_ptr<Buffer>> buffers;
   // list of passes
   std::vector<Pass> passes;
-
-  struct AllocatedResource
-  {
-	  bool isInUse = false;
-	  //variant<Texture,Buffer> 
-  };
-
-  // allocated resources
-  struct AllocatedTexture {
-	  Texture::Desc desc;
-	  Texture tex;
-	  int used = -1;
-  };
-
-  int getCompatibleResource(const Texture::Desc &desc, int passId) {
-    int index = 0;
-    for (auto &t : allocatedTextures) {
-      // check compatibility of descriptors
-      if (t.desc.dims == desc.dims && t.desc.width == desc.width &&
-          t.desc.height == desc.height && t.desc.depth == desc.depth &&
-          t.desc.fmt == desc.fmt && t.desc.sampleCount == desc.sampleCount &&
-          t.desc.mipMapCount == desc.mipMapCount) {
-        return index;
-      }
-      ++index;
-    }
-    // create a new one
-    allocatedTextures.push_back(AllocatedTexture{});
-    auto &newtex = allocatedTextures.back();
-    newtex.desc = desc;
-    newtex.used = passId;
-    newtex.tex = Texture{desc};
-    return (int)(allocatedTextures.size() - 1);
-  }
-
-  // we can keep the list from one frame to another,
-  // although I suspect that allocating memory for textures
-  // on the GPU should not be that costly
-  std::vector<AllocatedTexture> allocatedTextures;
-
-  // std::vector<Texture>
 };
 
 // Resource allocation:
--- src/Engine/FrameGraph.cpp
+++ src/Engine/FrameGraph.cpp
@@ -1,134 +1,156 @@
 #include <autograph/Engine/FrameGraph.h>
 
-namespace ag
-{
-	Texture::Desc FrameGraph::getTextureDesc(int handle) {
-		auto &rd = getResourceDesc(handle);
-		auto ptexdesc = ag::get_if<TextureResourceDesc>(rd->desc);
-		if (!ptexdesc)
-			ag::failWith("");
-	} 
+namespace ag {
+using ag::get_if;
 
-	FrameGraph::Resource FrameGraph::addResourceDesc(const ResourceDesc &rd) {
-		resources.push_back(std::make_unique<ResourceDesc>(rd));
-		int handle = (int)(resources.size() - 1);
-		AG_DEBUG("addResource {}.{}", handle, 0);
-		return Resource{ handle,0 };
-	}
-
-	FrameGraph::ResourceDesc &FrameGraph::getResourceDesc(int handle) {
-		return *resources[handle];
-	}
-
-	Texture::Desc& getTextureDesc(int handle) {
-		auto &rd = getResourceDesc();
-		auto ptexdesc = ag::get_if<TextureResourceDesc>(&rd.desc);
-		if (!ptexdesc)
-			ag::failWith("")
-	}
-
-
-	void FrameGraph::compile() {
-		//////////////////////////////////////////////
-		// Topological sorting
-		// by construction, 'passes' already contains
-		// a topological sort of the frame graph
-
-		//////////////////////////////////////////////
-		// concurrent resource writes detection and resource lifetime calculation
-		// (begin = pass that creates the resource, end = last pass that reads the
-		// resource)
-		bool hasWriteConflicts = false;
-		std::vector<int> r_ren; // read rename list (contains the rename index that
-								// should appear for the next read from a resource)
-		std::vector<int> w_ren; // write rename list (contains the rename index that
-								// should appear for the next write to a resource)
-		r_ren.resize(resources.size(), 0);
-		w_ren.resize(resources.size(), 0);
-
-		int pass_index = 0;
-		for (auto &p : passes) {
-			for (auto &r : p.reads) {
-				// ensure we can read this resource
-				if (r_ren[r.handle] > r.renameIndex) {
-					// we already bumped the rename index for this resource: it was
-					// already written to
-					warningMessage(
-						"[FrameGraph] read/write conflict detected on resource {}.{}",
-						r.handle, r.renameIndex);
-					hasWriteConflicts = true;
-				}
-				else {
-					AG_DEBUG("write rename index for {}: .{} -> .{}", r.handle, r.renameIndex, r.renameIndex + 1);
-					w_ren[r.handle] = r.renameIndex + 1;
-				}
-				auto &res = resources[r.handle];
-				// update lifetime end
-				// resource is read during this pass, so it must outlive it
-				if (res.lifetimeEnd < pass_index) {
-					res.lifetimeEnd = pass_index;
-					AG_DEBUG("lifetime of {}: pass {} -> {}", r.handle, res.lifetimeBegin, res.lifetimeEnd);
-				}
-			}
-
-			for (auto &w : p.writes) {
-				auto &res = resources[w.handle];
-				if (res.lifetimeBegin == -1) {
-					res.lifetimeBegin = pass_index;
-				}
-
-				// Note: p.writes should not contain the same resource twice with a
-				// different rename index
-				if (w_ren[w.handle] > w.renameIndex) {
-					warningMessage(
-						"[FrameGraph] read/write conflict detected on resource {}.{}",
-						w.handle, w.renameIndex);
-					hasWriteConflicts = true;
-				}
-				else {
-					// the next pass cannot use this rename for either read or write
-					// operations
-					AG_DEBUG("read rename index for {}: .{} -> .{}", w.handle, w.renameIndex, w.renameIndex + 1);
-					AG_DEBUG("write rename index for {}: .{} -> .{}", w.handle, w.renameIndex, w.renameIndex + 1);
-					r_ren[w.handle] = w.renameIndex + 1;
-					w_ren[w.handle] = w.renameIndex + 1;
-				}
-			}
-			++pass_index;
-		}
-
-		//////////////////////////////////////////////
-		// transient resource allocation
-		pass_index = 0;
-
-		for (auto &p : passes) {
-			// create resources that should be created
-			for (auto &c : p.creates) {
-				auto &r = resources[c.handle];
-				if (auto texres = ag::get_if<TextureResource>(&r.res)) {
-					// create the texture
-					texres->texId =
-						getCompatibleTextureResource(texres->desc, pass_index);
-				}
-				else {
-					// TODO other resources
-				}
-			}
-
-			++pass_index;
-
-			// release read-from resources that should be released on this pass
-			for (auto &read : p.reads) {
-				auto &r = resources[read.handle];
-				if (r.lifetimeEnd >= pass_index) {
-					if (auto texres = ag::get_if<TextureResource>(&r.res)) {
-						allocatedTextures[texres->texId].used = -1;
-					}
-					else {
-						// TODO other resources
-					}
-				}
-			}
-		}
-	}
+FrameGraph::Resource FrameGraph::addResourceDesc(const ResourceDesc &rd) {
+  resources.push_back(std::make_unique<ResourceDesc>(rd));
+  int handle = (int)(resources.size() - 1);
+  AG_DEBUG("addResource {}.{}", handle, 0);
+  return Resource{handle, 0};
 }
+
+const FrameGraph::ResourceDesc &FrameGraph::getResourceDesc(int handle) const {
+  return *resources[handle];
+}
+
+int FrameGraph::assignCompatibleResource(FrameGraph::ResourceDesc &desc,
+                                         int passId) {
+  int index = 0;
+  if (auto ptexdesc = get_if<ResourceDesc::Texture>(&desc.v)) {
+    for (int i = 0; i < textures.size(); ++i) {
+      if (isTextureInUse(i))
+        continue;
+      auto ptex = textures[i].get();
+      if (ptex->desc() == ptexdesc->desc) {
+        ptexdesc->ptex = ptex;
+        setTextureInUse(i);
+        return;
+      }
+    }
+    auto tex = std::make_unique<Texture>(ptexdesc->desc);
+    auto ptex = tex.get();
+    textures.push_back(std::move(tex));
+    ptexdesc->ptex = ptex;
+  } else if (auto pbufdesc = get_if<ResourceDesc::Buffer>(&desc.v)) {
+    /*for (int i = 0; i < buffers.size(); ++i)
+    {
+            if (isBufferInUse(i)) continue;
+            auto ptex = textures[i].get();
+            if (ptex->desc() == ptexdesc->desc) {
+                    ptexdesc->ptex = ptex;
+                    setTextureInUse(i);
+                    return;
+            }
+    }*/
+    // always create another buffer, for now
+    auto buf = std::make_unique<Buffer>(pbufdesc->size, pbufdesc->usage);
+    auto pbuf = buf.get();
+    buffers.push_back(std::move(buf));
+    pbufdesc->buf = pbuf;
+  }
+}
+
+void FrameGraph::compile() {
+  //////////////////////////////////////////////
+  // Topological sorting
+  // by construction, 'passes' already contains
+  // a topological sort of the frame graph
+
+  //////////////////////////////////////////////
+  // concurrent resource writes detection and resource lifetime calculation
+  // (begin = pass that creates the resource, end = last pass that reads the
+  // resource)
+  bool hasWriteConflicts = false;
+  std::vector<int> r_ren; // read rename list (contains the rename index that
+                          // should appear for the next read from a resource)
+  std::vector<int> w_ren; // write rename list (contains the rename index that
+                          // should appear for the next write to a resource)
+  r_ren.resize(resources.size(), 0);
+  w_ren.resize(resources.size(), 0);
+
+  int pass_index = 0;
+  for (auto &p : passes) {
+    for (auto &r : p.reads) {
+      // ensure we can read this resource
+      if (r_ren[r.handle] > r.renameIndex) {
+        // we already bumped the rename index for this resource: it was
+        // already written to
+        warningMessage(
+            "[FrameGraph] read/write conflict detected on resource {}.{}",
+            r.handle, r.renameIndex);
+        hasWriteConflicts = true;
+      } else {
+        AG_DEBUG("write rename index for {}: .{} -> .{}", r.handle,
+                 r.renameIndex, r.renameIndex + 1);
+        w_ren[r.handle] = r.renameIndex + 1;
+      }
+      auto &res = resources[r.handle];
+      // update lifetime end
+      // resource is read during this pass, so it must outlive it
+      if (res.lifetimeEnd < pass_index) {
+        res.lifetimeEnd = pass_index;
+        AG_DEBUG("lifetime of {}: pass {} -> {}", r.handle, res.lifetimeBegin,
+                 res.lifetimeEnd);
+      }
+    }
+
+    for (auto &w : p.writes) {
+      auto &res = resources[w.handle];
+      if (res.lifetimeBegin == -1) {
+        res.lifetimeBegin = pass_index;
+      }
+
+      // Note: p.writes should not contain the same resource twice with a
+      // different rename index
+      if (w_ren[w.handle] > w.renameIndex) {
+        warningMessage(
+            "[FrameGraph] read/write conflict detected on resource {}.{}",
+            w.handle, w.renameIndex);
+        hasWriteConflicts = true;
+      } else {
+        // the next pass cannot use this rename for either read or write
+        // operations
+        AG_DEBUG("read rename index for {}: .{} -> .{}", w.handle,
+                 w.renameIndex, w.renameIndex + 1);
+        AG_DEBUG("write rename index for {}: .{} -> .{}", w.handle,
+                 w.renameIndex, w.renameIndex + 1);
+        r_ren[w.handle] = w.renameIndex + 1;
+        w_ren[w.handle] = w.renameIndex + 1;
+      }
+    }
+    ++pass_index;
+  }
+
+  //////////////////////////////////////////////
+  // transient resource allocation
+  pass_index = 0;
+
+  for (auto &p : passes) {
+    // create resources that should be created
+    for (auto &c : p.creates) {
+      auto &r = resources[c.handle];
+      if (auto texres = ag::get_if<TextureResource>(&r.res)) {
+        // create the texture
+        texres->texId = getCompatibleTextureResource(texres->desc, pass_index);
+      } else {
+        // TODO other resources
+      }
+    }
+
+    ++pass_index;
+
+    // release read-from resources that should be released on this pass
+    for (auto &read : p.reads) {
+      auto &r = resources[read.handle];
+      if (r.lifetimeEnd >= pass_index) {
+        if (auto texres = ag::get_if<TextureResource>(&r.res)) {
+          allocatedTextures[texres->texId].used = -1;
+        } else {
+          // TODO other resources
+        }
+      }
+    }
+  }
+}
+} // namespace ag
